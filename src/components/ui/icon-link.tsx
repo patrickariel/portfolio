359691e38ea9b08f1c0da472fb046e8f79b93a6e
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-08-02 18:32:09 +0700

    feat: implement icon link component

---
+++ src/components/ui/icon-link.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { ExternalLink, type LucideIcon } from "lucide-react";
+import React from "react";
+
+export const IconLink = React.forwardRef<
+  HTMLAnchorElement,
+  React.AnchorHTMLAttributes<HTMLAnchorElement> & { icon?: LucideIcon }
+>(({ className, icon: Icon = ExternalLink, children, ...props }, ref) => (
+  <a
+    ref={ref}
+    className={cn("group flex w-fit flex-row items-center gap-2", className)}
+    {...props}
+  >
+    <span className="group-hover:underline">{children}</span> <Icon size={15} />
+  </a>
+));
+IconLink.displayName = "IconLink";
